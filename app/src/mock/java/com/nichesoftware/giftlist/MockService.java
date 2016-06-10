a3c70ed726abf7eabe7a851cc118ad9d395a6251
n_che <nicolas.cherin@infotel.com>
2016-06-10 16:44:50 +0200

    - Person <> Room - Gestion cache (à revoir) - Ajout d'une page de démarrage avec didacticiel (authent/enregistrement à gérer) - Ajout de salle - Ajout de cadeau - Détail d'un cadeau (en cours)

--- app/src/mock/java/com/nichesoftware/giftlist/MockService.java
+++ app/src/mock/java/com/nichesoftware/giftlist/MockService.java
@@ -1,7 +1,9 @@
 package com.nichesoftware.giftlist;
 
+import android.util.Log;
+
 import com.nichesoftware.giftlist.model.Gift;
-import com.nichesoftware.giftlist.model.Person;
+import com.nichesoftware.giftlist.model.Room;
 import com.nichesoftware.giftlist.service.ServiceAPI;
 
 import java.util.ArrayList;
@@ -11,24 +13,65 @@ import java.util.List;
  * Created by n_che on 25/04/2016.
  */
 public class MockService implements ServiceAPI {
-    private static List<Person> PERSONS = new ArrayList<>();
+    private final static String TAG = MockService.class.getSimpleName();
+
+    private static List<Room> ROOMS = new ArrayList<>();
+    private static List<Gift> GIFTS = new ArrayList<>();
 
     static {
-        List<Gift> gifts = new ArrayList<>();
         Gift gift = new Gift();
         gift.setName("Ours en peluche");
-        gift.setAmount(22.05);
-        gifts.add(gift);
+        gift.setPrice(22.05);
+        GIFTS.add(gift);
+
         gift = new Gift();
         gift.setName("Playstation 8");
-        gift.setAmount(455.99);
-        gifts.add(gift);
-        PERSONS.add(new Person("0", "John", "Doe", gifts));
-        PERSONS.add(new Person("1", "Jane", "Doe"));
-        PERSONS.add(new Person("2", "Jean-Charles", "Dupond"));
+        gift.setPrice(455.99);
+        GIFTS.add(gift);
+
+        ROOMS.add(new Room(0, "John Doe", "Anniversaire"));
+        ROOMS.add(new Room(1, "Jane Doe", "Noël"));
+        ROOMS.add(new Room(2, "Jean-Charles Dupond", "Pot de départ"));
     }
+
     @Override
-    public void getAllPersons(PersonServiceCallback<List<Person>> callback) {
-        callback.onLoaded(PERSONS);
+    public void authenticate(String username, String password, ServiceCallback<String> callback) {
+        callback.onLoaded("");
+    }
+
+    @Override
+    public void register(String username, String password, ServiceCallback<String> callback) {
+        callback.onLoaded("");
+    }
+
+    @Override
+    public void getAllRooms(final String token, ServiceCallback<List<Room>> callback) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, String.format("getAllRooms [token = %s]", token));
+        }
+        callback.onLoaded(ROOMS);
+    }
+
+    @Override
+    public void getGifts(String token, int roomId, ServiceCallback<List<Gift>> callback) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, String.format("getGifts [token = %s, roomId = %d]", token, roomId));
+        }
+        callback.onLoaded(GIFTS);
+    }
+
+    @Override
+    public void createRoom(String token, String roomName, String occasion, ServiceCallback<Boolean> callback) {
+        callback.onLoaded(true);
+    }
+
+    @Override
+    public void addGift(String token, int roomId, String name, double price, double amount, ServiceCallback<Boolean> callback) {
+        callback.onLoaded(true);
+    }
+
+    @Override
+    public void updateGift(String token, int roomId, int giftId, double amount, ServiceCallback<Boolean> callback) {
+        callback.onLoaded(true);
     }
 }
